9ce22a66d7746addb977babc2772c2faf751b580
yongseopK <kk0@kakao.com>
2024-01-10 10:48:27 +0900

    #2. TodoTemplate 컴포넌트 분리, SCSS 작성

--- src/component/TodoTemplate.js
+++ src/component/TodoTemplate.js
@@ -1,55 +1,17 @@
 import React from "react";
 import {MdAdd, MdDone, MdDelete} from "react-icons/md";
+import './scss/TodoTemplate.scss'
+import TodoHeader from "./TodoHeader";
+import TodoInput from "./TodoInput";
+import TodoMain from "./TodoMain";
 
 const TodoTemplate = () => {
     return (
-        <>
-            <header>
-                <h1>2024년 1월 10일</h1>
-                <div className="day">수요일</div>
-                <div className="tasks-left">할 일 3개 남음</div>
-            </header>
-            <ul className="todo-list">
-                <li className='todo-list-item'>
-                    <div className='check-circle'>
-                        <MdDone/>
-                    </div>
-                    <span className='text'>할 일 어쩌구~~</span>
-                    <div className='remove'>
-                        <MdDelete/>
-                    </div>
-                </li>
-                <li className='todo-list-item'>
-                    <div className='check-circle'>
-                        <MdDone/>
-                    </div>
-                    <span className='text'>할 일 어쩌구~~</span>
-                    <div className='remove'>
-                        <MdDelete/>
-                    </div>
-                </li>
-                <li className='todo-list-item'>
-                    <div className='check-circle'>
-                        <MdDone/>
-                    </div>
-                    <span className='text'>할 일 어쩌구~~</span>
-                    <div className='remove'>
-                        <MdDelete/>
-                    </div>
-                </li>
-                <div className='form-wrapper'>
-                    <form className='insert-form'>
-                        <input
-                            type='text'
-                            placeholder='할 일을 입력 후, 엔터를 누르세요!'
-                        />
-                    </form>
-                </div>
-                <button className='insert-btn'>
-                    <MdAdd/>
-                </button>
-            </ul>
-        </>
+        <div className='TodoTemplate'>
+            <TodoHeader/>
+            <TodoMain />
+            <TodoInput/>
+        </div>
     );
 };
 
