901f692042757254e46caf5b84c9526cc67dff2f
yongseopK <kk0@kakao.com>
2024-01-11 09:28:58 +0900

    #5. TodoItem 체크 스타일 동적 클래스 변경

--- src/component/TodoItem.js
+++ src/component/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {MdDelete, MdDone} from "react-icons/md";
 import "./scss/TodoItem.scss";
+import cn from 'classnames';
 
 const TodoItem = ({item}) => {
 
@@ -8,10 +9,10 @@ const TodoItem = ({item}) => {
 
     return (
         <li className='todo-list-item'>
-            <div className='check-circle'>
+            <div className={cn('check-circle', {active: done})}>
                 {done && <MdDone/>}
             </div>
-            <span className='text'>{title}</span>
+            <span className={cn('text', {finish: done})}>{title}</span>
             <div className='remove'>
                 <MdDelete/>
             </div>
