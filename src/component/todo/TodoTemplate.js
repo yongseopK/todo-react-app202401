a4d7bc58eecc4a85d50a18fc479bb9e25cb67634
yongseopK <kk0@kakao.com>
2024-01-25 11:19:13 +0900

    #15. todo list 헤더 인증토큰 넣기

--- src/component/todo/TodoTemplate.js
+++ src/component/todo/TodoTemplate.js
@@ -4,14 +4,23 @@ import './scss/TodoTemplate.scss'
 import TodoHeader from "./TodoHeader";
 import TodoInput from "./TodoInput";
 import TodoMain from "./TodoMain";
-import axios from "axios";
 
 import { TODO_URL } from "../../config/host-config";
+import {getCurrentLoginUser} from "../../util/login-util";
 
 const TodoTemplate = () => {
 
+    // 토큰 가져오기
+    const [token, setToken] = useState(getCurrentLoginUser().token);
+
     // 서버에서 할 일 목록 (JSON)을 요청해서 받아와야 함
     const API_BASE_URL = TODO_URL;
+
+    // 요청 헤더 객체
+    const requestHeader = {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${token}`
+    };
 
     // 렌더링 직전에 해야할 코드를 적는 함수
     useEffect(() => {
@@ -23,7 +32,10 @@ const TodoTemplate = () => {
             //     console.error(error);
             // }
 
-            fetch(API_BASE_URL)
+            fetch(API_BASE_URL, {
+                method: "GET",
+                headers: requestHeader
+            })
                 .then(res => res.json())
                 .then(json => {
                     setTodoList(json.todos);
@@ -64,9 +76,7 @@ const TodoTemplate = () => {
          */
         fetch(API_BASE_URL, {
             method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
+            headers: requestHeader,
             body: JSON.stringify(newTodo)
         })
             .then(res => res.json())
@@ -81,6 +91,7 @@ const TodoTemplate = () => {
 
         fetch(`${API_BASE_URL}/${id}`, {
             method: "delete",
+            headers: requestHeader
         }).then(res => res.json())
             .then(json => {
                 setTodoList(json.todos);
@@ -100,9 +111,7 @@ const TodoTemplate = () => {
 
         fetch(API_BASE_URL, {
             method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
+            headers: requestHeader,
             body: JSON.stringify({
                 id: id,
                 done: !done
